fc4e67a392bcb459d820aa487883d089f01188a6
xiaoanliu <864431514@qq.com>
2022-09-20 19:24:24 +0800

    11111111

---
+++ src/main/java/org/example/student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author: Liuag
+ * @Project: JavaTraining
+ * @Pcakage: org.example.student
+ * @Date: 2022年09月20日 19:22
+ * @Description:
+ */
+public class student {
+    private  String sname;
+
+}

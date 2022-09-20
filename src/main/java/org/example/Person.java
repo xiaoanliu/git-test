40a404218d1579541e081ebbd2a0b431d9056c99
xiaoanliu <864431514@qq.com>
2022-09-20 19:01:51 +0800

    Initial commit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author: Liuag
+ * @Project: JavaTraining
+ * @Pcakage: org.example.Person
+ * @Date: 2022年09月20日 18:52
+ * @Description:
+ */
+public class Person {
+    private  String id;
+
+}
